f146ac564a2e091bd152de4c46cec8af398c4c16
Elvis Cueto <cueto@MacBook-Air-de-Elvis.local>
2019-02-28 15:16:36 -0600

    Bucador afiliado

--- afiliado/views/js/script.js
+++ afiliado/views/js/script.js
@@ -60,32 +60,38 @@ if ( table ){
 }
 
 const search = document.getElementById('search-item');
-
 (function(){
   if ( search ){
     search.addEventListener('keyup', function(){
-      let value = search.value.toLowerCase().trim();
-      const items = document.querySelector('.containerDatosClientesReferido').querySelectorAll('.datosCliente-container');
-      let length = value.length;
-
-      if(  length > 0 ) {
-        items.forEach( (item) => {
-          let type = item.dataset.item;
-
-          let match = type.slice(0, length);
-          //console.log(type, length, search.value)
-
-          if ( value === match ){
-          }
-          else{
-            item.style.display = 'none';
-          }
-        })
-
-       } else {
-        items.forEach( (item) => { item.style.display = ''})
-       }
-
+    	//const items = document.querySelector('.containerDatosClientesReferido').querySelectorAll('.datosCliente-container');
+        var tableReg =document.querySelectorAll('.table-responsive');     
+    	var searchText = search.value.toLowerCase().trim();
+    	
+    	for(var searching=0; searching < tableReg.length ; searching++) {
+    		var cellsOfRow="";
+    		var found=false;
+    		var compareWith="";
+    		for (var i = 1; i < tableReg[searching].rows.length; i++) {
+    			cellsOfRow = tableReg[searching].rows[i].getElementsByTagName('td');
+    			found = false;
+    			for (var j = 0; j < cellsOfRow.length && !found; j++){
+    				compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+    				// Buscamos el texto en el contenido de la celda
+    				if (searchText.length == 0 || (compareWith.indexOf(searchText) > -1)){
+    					found = true;
+    				}
+    			}
+    			//console.log(found)
+    			//console.log(tableReg[searching].parentElement.parentElement)
+    			if(found){
+    				tableReg[searching].parentElement.parentElement.style.display = '';
+    			} else {
+    				// si no ha encontrado ninguna coincidencia, esconde la
+    				// fila de la tabla
+    				tableReg[searching].parentElement.parentElement.style.display = 'none';
+    			}
+    		}
+    	}
     })
   }
 })();
