8463713bacf280b0947b35f3bfd76cdacbfc66fa
Elvis Cueto <cueto@MacBook-Air-de-Elvis.local>
2019-03-02 01:58:01 -0600

    Iframe recursos afiliado

--- afiliado/views/modules/archivos.php
+++ afiliado/views/modules/archivos.php
@@ -9,13 +9,23 @@ require "views/modules/navegacionStart.php";
 require "views/modules/navegacionEnd.php";
 ?>
 
+<style>
+iframe {
+    max-width: 100%;
+    width:100%; 
+    height:600px; 
+    border:0;
+}
+</style>
 <div class="mainReset-content">
   <div class="title-content">
     <h1 class="title__titulo"><?php echo $_GET["action"]; ?></h1>
-    <form class="buscar" name="titulo" action="" method="get">
+    <!--<form class="buscar" name="titulo" action="" method="get">
       <input class="filtro" type="text" placeholder="Producto..." id="search-item">
       <button type="submit" class="icono fa fa-search"></button>
-    </form>
+    </form>--> 
   </div>
+  
+  <iframe allowfullscreen src="https://drive.google.com/embeddedfolderview?id=1u7EvMeTmnj2mQMAQdg38y9niKTLhg1Nj#grid""></iframe>
 
 </div>
